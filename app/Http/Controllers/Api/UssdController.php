cbf8fd738b9be12d6e6bd535a3c46da85d54b8f8
Julian <brandontabona@gmail.com>
2019-12-10 13:38:12 +0200

    update

--- app/Http/Controllers/Api/UssdController.php
+++ app/Http/Controllers/Api/UssdController.php
@@ -266,7 +266,7 @@ class UssdController extends Controller
             }
         }
 
-        $response = \App\Order::find(174)->invoces()->first()->smsInvoiceReceiptToCustomer();
+        $response = \App\Order::find(174)->invoices()->first()->smsInvoiceReceiptToCustomer();
 
         /*  Return the response to the user  */
         //return response($response)->header('Content-Type', 'text/plain');
